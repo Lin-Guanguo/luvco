20a2d0cc42fb8a7cef3fe28f8333cd3753169ee8
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-03-19 11:24:26 +0800

    add connection write

--- src/net.c
+++ src/net.c
@@ -43,8 +43,13 @@ typedef struct tcp_server {
 typedef struct tcp_connection {
     uv_tcp_t tcp;
     lua_State* L;
-    char* read_buf;
     bool closed;
+
+    char* read_buf; // read buf, alloc in first read, free in gc
+
+    uv_write_t* write_req;
+    uv_buf_t* write_bufs; // array of buf, wait to write, alloc in write, free in gc
+    int write_bufs_n;
 } tcp_connection;
 
 static void server_accept_cb (uv_stream_t* tcp, int status);
@@ -89,8 +94,11 @@ static int server_accept (lua_State* L) {
 
     tcp_connection* client = luvco_pushudata_with_meta(L, tcp_connection);
     client->L = L;
-    client->read_buf = NULL;
     client->closed = false;
+    client->read_buf = NULL;
+    client->write_req = NULL;
+    client->write_bufs = NULL;
+    client->write_bufs_n = 0;
     int ret = uv_tcp_init(&state->loop, (uv_tcp_t *)client);
     assert(ret == 0);
 
@@ -135,7 +143,6 @@ static int connection_read_k (lua_State *L, int status, lua_KContext ctx);
 
 static int connection_read (lua_State* L) {
     tcp_connection* con = luvco_check_udata(L, 1, tcp_connection);
-    luvco_state* state = luvco_get_state(L);
     log_trace("connection %p read", con);
 
     int ret = uv_read_start((uv_stream_t*)con, connection_alloc_cb, connection_read_cb);
@@ -169,6 +176,50 @@ static void connection_read_cb (uv_stream_t* stream, ssize_t nread, const uv_buf
     // TODO
 }
 
+static void connection_write_cb (uv_write_t* req, int status);
+static int connection_write_k (lua_State* L);
+
+static int connection_write (lua_State* L) {
+    tcp_connection* con = luvco_check_udata(L, 1, tcp_connection);
+    luvco_state* state = luvco_get_state(L);
+    log_trace("connection %p write", con);
+
+    int top = lua_gettop(L);
+    int write_n = top - 1;
+    if (con->write_bufs_n < write_n) {
+        con->write_bufs = (uv_buf_t*)realloc(con->write_bufs, sizeof(uv_buf_t) * write_n);
+        con->write_bufs_n = write_n;
+    }
+    for (int i = 2; i <= top; ++i) {
+        size_t len;
+        char* s = (char*)luaL_checklstring(L, i, &len);
+        con->write_bufs[i-2].base = s;
+        con->write_bufs[i-2].len = len;
+    }
+    if (con->write_req == NULL) {
+        con->write_req = (uv_write_t*)malloc(sizeof(uv_write_t));
+        ASSERT_NOT_NULL(con->write_req);
+    }
+    int ret = uv_write(con->write_req, (uv_stream_t*)con, con->write_bufs, write_n, connection_write_cb);
+    assert(ret == 0);
+    lua_yieldk(L, 0, (lua_KContext)NULL, connection_write_k);
+}
+
+static int connection_write_k (lua_State* L) {
+    return 1;
+}
+
+static void connection_write_cb (uv_write_t* req, int status) {
+    tcp_connection* con = (tcp_connection*)req->handle;
+    if (status != 0) {
+        log_error("connection %p write error, status %d", con, status);
+    }
+    lua_State* L = con->L;
+    lua_pushinteger(L, status);
+    int res;
+    luvco_resume(L, 1, &res);
+}
+
 static void connection_close_cb (uv_handle_t* handle);
 
 static int connection_close (lua_State *L) {
@@ -190,6 +241,8 @@ static int connection_gc (lua_State *L) {
     log_trace("connection %p gc", con);
     connection_close(L);
     free(con->read_buf);
+    free(con->write_req);
+    free(con->write_bufs);
     return 0;
 }
 
@@ -209,6 +262,7 @@ static const luaL_Reg server_m [] = {
 
 static const luaL_Reg con_m [] = {
     { "read", connection_read },
+    { "write", connection_write },
     { "close", connection_close },
     { "__gc", connection_gc },
     { NULL, NULL}

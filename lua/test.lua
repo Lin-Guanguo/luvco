51ea9b8e30e9906a12461511444887d62be95078
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-03-19 11:26:09 +0800

    add echo server example

--- lua/test.lua
+++ lua/test.lua
@@ -6,10 +6,16 @@ return function()
             local addr = luvco.net.new_ip4_addr("127.0.0.1", 8080)
             local server = luvco_net.new_server(addr)
             local connection = server:accept()
-            local reads = connection:read()
+
+            repeat
+                local reads = connection:read()
+                print( reads )
+                local write_ret = connection:write( "hello echo=\"", reads, "\"\n")
+                print( "write ret", write_ret)
+            until string.sub(reads, 1, 4) == "quit"
+
             connection:close()
             server:close()
-            print( reads )
         end
         collectgarbage("collect");
         luvco._free_co()

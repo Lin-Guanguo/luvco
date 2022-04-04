17209d70840b11241064ce4926377e74f9ae802d
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-04-04 10:25:21 +0800

    Add test ispawn, concurrent echo-server

--- lua/test.lua
+++ lua/test.lua
@@ -1,17 +1,50 @@
+
+luvco.ispawn(
+    --function()
+    [[
+luvco.import("lualibs")
+local net = luvco.import("net");
+
+local addr = luvco.net.new_ip4_addr("127.0.0.1", 8081)
+local server = net.new_server(addr)
+
+while true do
+    local connection = server:accept()
+    if connection == nil then
+        break
+    end
+
+    print ("lua: accpet return", connection)
+    luvco.spawn(function()
+        repeat
+            local reads = connection:read()
+            if #reads == 0 then
+                print ("read eof")
+                break
+            end
+
+            local write_ret = connection:write(reads)
+            if string.sub(reads, 1, 5) == "close" then
+                server:close();
+            elseif string.sub(reads, 1, 2) == "gc" then
+                collectgarbage("collect");
+            end
+        until string.sub(reads, 1, 4) == "quit"
+        connection:close()
+    end)
+end
+
+server:close()
+print("accept when close", server:accept())
+    ]]
+    --end
+)
+
 luvco.import("lualibs")
 local net = luvco.import("net");
 
 local addr = luvco.net.new_ip4_addr("127.0.0.1", 8080)
 local server = net.new_server(addr)
-
-luvco.ispawn(
-    --function()
-    [[
-    luvco.import("lualibs")
-    print ("hello in new lua state by ispawn")
-    ]]
-    --end
-)
 
 while true do
     local connection = server:accept()

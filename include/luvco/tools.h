94f8801b27fbe2db7509c4d7baec3466dbff3237
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-04-04 17:39:28 +0800

    Add object system, obj header

--- include/luvco/tools.h
+++ include/luvco/tools.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <assert.h>
 #include <stdbool.h>
+#include <string.h>
 
+#include <lua/lua.h>
+#include <lua/lauxlib.h>
 #include <luvco/log.h>
+#include <luvco.h>
 
 #define luvco_new_meta(L, type)          \
     luaL_newmetatable((L), "luvco."#type);    \
@@ -17,8 +21,6 @@
     (type*)luaL_checkudata((L), (n), "luvco."#type)
 
 #define container_of(ptr, type, member) (type*)((char*)(ptr) - (char*)(&(((type*)NULL)->member)))
-
-typedef lua_State* (*luvco_newluaf) (void* ud);
 
 typedef struct luvco_state {
     uv_loop_t loop;
@@ -38,3 +40,22 @@ void luvco_yield (lua_State *L, lua_KContext k_ctx, lua_KFunction k);
 void luvco_resume (lua_State *L, int nargs);
 
 void luvco_dump_lua_stack (lua_State *L);
+
+
+
+#define luvco_objheader unsigned char moved : 1;
+
+#define luvco_init_objheader(obj) (obj)->moved = false;
+
+#define luvco_checkmoved(L, obj) \
+    if ((obj)->moved) { \
+        log_error("luvco obj %p is moved", (obj)); \
+        luaL_error(L, "luvco obj %p is moved", (obj)); \
+    }
+
+#define luvco_moveudata(L, NL, type) { \
+        type* to = luvco_pushudata_with_meta(NL, type); \
+        type* from = lua_touserdata(L, -1); \
+        memcpy(to, from, sizeof(type)); \
+        from->moved = true; \
+    }
